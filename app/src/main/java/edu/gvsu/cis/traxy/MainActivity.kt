6e14230f7d887cd8bd9d176fb35e2a47cf7b4652
Hans Dulimarta <hans.dulimarta@gmail.com>
2020-08-22 18:22:45 -0400

    Add Logout menu

--- app/src/main/java/edu/gvsu/cis/traxy/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/traxy/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.gvsu.cis.traxy
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -15,5 +17,18 @@ class MainActivity : AppCompatActivity() {
             val email = intent.getStringExtra("USER_EMAIL");
             userEmail.text = email;
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_logout) {
+            finish()
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/edu/gvsu/cis/traxy/SignUpActivity.kt
+++ app/src/main/java/edu/gvsu/cis/traxy/SignUpActivity.kt
@@ -3,11 +3,11 @@ package edu.gvsu.cis.traxy
 import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.content_sign_up.*
+import kotlinx.android.synthetic.main.content_sign_up.email
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
@@ -19,34 +19,41 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-fab.setOnClickListener { _ ->
-        val emailStr = email.text.toString()
-        val pass1Str = password1.text.toString()
-        val pass2Str = password2.text.toString()
-        val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
-        when {
-            emailStr.length == 0 ->
-                Snackbar
-                    .make(email, getString(R.string.email_requirred), Snackbar.LENGTH_LONG)
-                    .show()
-            !EMAIL_REGEX.matcher(emailStr).find() ->
-                Snackbar
-                    .make(email, getString(R.string.invalid_email), Snackbar.LENGTH_LONG)
-                    .show()
-            !pass1Str.contains("traxy") || !pass1Str.equals(pass2Str)->
-                fab.startAnimation(shake)
+        fab.setOnClickListener { view ->
+            val emailStr = email.text.toString()
+            val pass1Str = password1.text.toString()
+            val pass2Str = password2.text.toString()
 
-            else -> {
-                val nextPlz = Intent(this@SignUpActivity, MainActivity::class.java)
-                nextPlz.putExtra("USER_EMAIL", emailStr);
-                finish()
-                startActivity(nextPlz)
+            when {
+                emailStr.length == 0 ->
+                    Snackbar
+                        .make(email, getString(R.string.email_requirred), Snackbar.LENGTH_LONG)
+                        .show()
+                !EMAIL_REGEX.matcher(emailStr).find() -> 
+                    Snackbar
+                        .make(email, getString(R.string.invalid_email), Snackbar.LENGTH_LONG)
+                        .show()
+                pass1Str.length == 0 || pass2Str.length == 0 ->
+                    Snackbar
+                        .make(email, getString(R.string.empty_password), Snackbar.LENGTH_LONG)
+                        .show()
+                !pass1Str.equals(pass2Str) ->
+                    Snackbar
+                        .make(email, getString(R.string.unmatch_password), Snackbar.LENGTH_LONG)
+                        .show()
+
+                !pass1Str.contains("traxy") ->
+                    Snackbar
+                            .make(password1, getString(R.string.incorrect_password), Snackbar.LENGTH_LONG)
+                            .show()
+
+                else -> {
+                    val nextPlz = Intent(this@SignUpActivity, MainActivity::class.java)
+                    nextPlz.putExtra("USER_EMAIL", emailStr);
+                    finish()    // Teerminate this activity and remove it from the Activity stack
+                    startActivity(nextPlz)
+                }
             }
         }
-    {}}
     }
 }
