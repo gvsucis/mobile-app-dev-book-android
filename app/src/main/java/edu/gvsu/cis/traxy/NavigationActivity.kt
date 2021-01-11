22a3f21e55f9003606ef86d2ae3ff928359cf24b
Hans Dulimarta <hans.dulimarta@gmail.com>
2021-01-11 14:41:53 -0500

    Update NavigationActivity to support 'Up' button

--- app/src/main/java/edu/gvsu/cis/traxy/NavigationActivity.kt
+++ app/src/main/java/edu/gvsu/cis/traxy/NavigationActivity.kt
@@ -2,12 +2,26 @@ package edu.gvsu.cis.traxy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class NavigationActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
+        val nc = findNavController(R.id.my_nav_host_fragment)
+        appBarConfig = AppBarConfiguration(nc.graph)
+        setupActionBarWithNavController(nc, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val nc = findNavController(R.id.my_nav_host_fragment)
+        return nc.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
