f2b0a69b43fec7eda9c59835deeb5bac9202ce4e
Hans Dulimarta <hans.dulimarta@gmail.com>
2021-01-13 13:34:59 -0500

    Add map markers to maps

--- app/src/main/java/edu/gvsu/cis/traxy/view/TripMapsFragment.kt
+++ app/src/main/java/edu/gvsu/cis/traxy/view/TripMapsFragment.kt
@@ -6,29 +6,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import edu.gvsu.cis.traxy.R
+import edu.gvsu.cis.traxy.viewmodel.UserDataViewModel
+import kotlinx.android.synthetic.main.fragment_trip_maps.*
 
 class TripMapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private val viewmodel by activityViewModels<UserDataViewModel>()
+    private var gMap: GoogleMap? = null
+    private var allMarkers = mutableListOf<MarkerOptions>()
+    private var mapBound = LatLngBounds.Builder()
+
+    private val callback = OnMapReadyCallback {
+        gMap = it
+        with(gMap!!.uiSettings) {
+            isZoomControlsEnabled = true
+            isZoomGesturesEnabled = true
+            isScrollGesturesEnabled = true
+            isScrollGesturesEnabledDuringRotateOrZoom = true
+        }
+        if (allMarkers.size > 0)
+            refreshMarkers(gMap!!, allMarkers)
     }
 
     override fun onCreateView(
@@ -39,9 +47,33 @@ class TripMapsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_trip_maps, container, false)
     }
 
+    private fun refreshMarkers(theMap: GoogleMap, theJournal: List<MarkerOptions>) {
+        theMap.clear()
+        theJournal.forEach {
+            mapBound.include(it.position)
+            theMap.addMarker(it)
+        }
+
+        val screenWidth = requireActivity().resources.displayMetrics.widthPixels
+        val screenHeight = requireActivity().resources.displayMetrics.heightPixels
+        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(mapBound.build(),
+            screenWidth, screenHeight, 128)
+        theMap.animateCamera(cameraUpdate)
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        viewmodel.remoteJournals.observe(viewLifecycleOwner, Observer {
+            it.forEach {
+                val pos = LatLng(it.lat, it.lng)
+                allMarkers.add(MarkerOptions().position(pos).title(it.name))
+            }
+            if (gMap != null)
+                refreshMarkers(gMap!!, allMarkers)
+
+        })
     }
 }
